880748a4d5664f89a560c758ebb2984801d3a842
andretokumoto <andretokumoto@gmail.com>
2026-07-05 22:55:23 -0300

    novo path

--- plataformaResumosAPP/urls.py
+++ plataformaResumosAPP/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import login_view,cadastro_view,dashboard_aluno,dashboard_professor,dashboard_coordenador,logout_view ,criar_turma_view,inscrever_em_turma_view,relatorio_view,submissao_view,painel_turma_view,projeto_analise_view,turmas_coordenador_view,projetos_aprovados_view,projeto_leitura_view,pdf_resumo_view, gera_revista_view,gerencia_tipo_usuario_view
 
 urlpatterns = [
+    path('', login_view, name='login'),
     path('login/', login_view, name='login'),
     path('cadastro/', cadastro_view, name='cadastro'),
     path('logout/', logout_view, name='logout'),
